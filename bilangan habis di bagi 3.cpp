a7c6e8e399ac95e7033b853e3f52b67dc4a70b5f
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 17:13:51 +0700

    Add files via upload

---
+++ bilangan habis di bagi 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+int x;
+cout<<"Menentukan apakah bilangan yang diinputkan habis di bagi 3 dan 5 atai tidak."<<endl;
+cout<<"input bilangan :";
+cin>>x;
+if (x%3=0);
+    cout<<"Bilangan tersebut habis di bagi 3 dan 5"<<endl;
+return 0;
+}
